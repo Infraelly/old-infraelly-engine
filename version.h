a0438fa39aade1ea8dbeaf6be8186770cad8ea5b
insanepotato <insanepotato@users.sourceforge.net>
2011-02-04 00:45:10 +1100

    *fixes for windows

--- version.h
+++ version.h
@@ -4,7 +4,7 @@
 namespace AutoVersion{
 	
 	//Date Version Types
-	static const char  _DATE[] = "03";
+	static const char  _DATE[] = "04";
 	static const char  _MONTH[] = "02";
 	static const char  _YEAR[] = "2011";
 	static const char  _UBUNTU_VERSION_STYLE[] = "11.02";
@@ -16,14 +16,14 @@ namespace AutoVersion{
 	//Standard Version Type
 	static const long  _MAJOR = 0;
 	static const long  _MINOR = 0;
-	static const long  _BUILD = 11294;
-	static const long  _REVISION = 11294;
+	static const long  _BUILD = 11295;
+	static const long  _REVISION = 11295;
 	
 	//Miscellaneous Version Types
-	static const long  _BUILDS_COUNT = 16157;
-	#define  _RC_FILEVERSION 0,0,11294,11294
-	#define  _RC_FILEVERSION_STRING "0, 0, 11294, 11294\0"
-	static const char  _FULLVERSION_STRING[] = "0.0.11294.11294";
+	static const long  _BUILDS_COUNT = 16176;
+	#define  _RC_FILEVERSION 0,0,11295,11295
+	#define  _RC_FILEVERSION_STRING "0, 0, 11295, 11295\0"
+	static const char  _FULLVERSION_STRING[] = "0.0.11295.11295";
 	
 	//These values are to keep track of your versioning state, don't modify them.
 	static const long  _BUILD_HISTORY = 0;
